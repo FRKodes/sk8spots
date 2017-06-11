805cd9567d9f96c0470674cc1120b3734512723e
FRKodes <frkalderon@gmail.com>
2017-06-11 17:23:43 -0500

    some fix in authserviceprovider to see if can push to heroku

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,11 +27,11 @@ class AuthServiceProvider extends ServiceProvider
 	{
 		$this->registerPolicies($gate);
 
-		foreach ($this->getPermissions() as $permission) {
-			$gate->define($permission->name, function($user) use ($permission) {
-				return $user->hasRole($permission->roles);
-			});
-		}
+		// foreach ($this->getPermissions() as $permission) {
+		// 	$gate->define($permission->name, function($user) use ($permission) {
+		// 		return $user->hasRole($permission->roles);
+		// 	});
+		// }
 	}
 
 	private function getPermissions()
