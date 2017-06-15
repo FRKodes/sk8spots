8f4a41548b9b5d325b8b57b3b645fcc02ded6170
frkodes <frkalderon@gmail.com>
2017-06-15 18:34:46 -0500

    Some dummy changes to deploy in order to update buildpack

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,15 +7,10 @@
 	<title>Sk8spotsMx</title>
 </head>
 <body>
-
 	<header>@include('partials.nav')</header>
-
 	@include('partials.flash-message')
-	
 	<section>@yield('content')</section>
-
 	<section>@include('partials.adsense')</section>
-
 	<footer>@include('partials.footer')</footer>
 	@include('partials.google')
 	@include('partials.hotjar')
@@ -26,6 +21,5 @@
 	@else
 		<script async defer src="//maps.googleapis.com/maps/api/js?key=AIzaSyAP9skepfDL3L5pB4oF0Y1eey1BdF2trcI&amp;callback=initMapDrag"></script>
 	@endif
-	
 </body>
 </html>
