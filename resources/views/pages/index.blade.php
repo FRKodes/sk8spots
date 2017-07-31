9d07d8edb6311e8002f31594561ffa7175c899ec
FRKodes <frkalderon@gmail.com>
2017-07-30 23:49:26 -0500

    some CSS fixes

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -13,15 +13,24 @@
 			<a href="{{ url('spot/create') }}" class="btn btn-primary green-btn share" title="¡El conocimiento es poder!">¡Compártelo con la comunidad!</a>
 		</div>
 	</div>
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2 adsense m-bottom-0">
+			<ins class="adsbygoogle"
+			     style="display:block"
+			     data-ad-client="ca-pub-6490955290269477"
+			     data-ad-slot="1652665079"
+			     data-ad-format="auto"></ins>
+			<script>
+			(adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
+		</div>
+	</div>
 </div>
-
-
-<section>@include('partials.adsense')</section>
 
 
 <div class="container spots-list">
 	<div class="row">
-		<h2 class="text-center verde2">¡Últimos Sk8 Spots agregados!</h2>
+		<h2 class="text-center verde2 m-top-0 m-bottom-20">¡Últimos Sk8 Spots agregados!</h2>
 		@if($lastOnes)
 			@foreach($lastOnes as $lastOne)
 				<div class="col-sm-4 col-md-3 last-one-home-item">
