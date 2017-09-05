a3d8f45aaea6aecdfcb3fc3d1e3531f05a003295
frkodes <frkalderon@gmail.com>
2017-09-05 12:04:40 -0500

    Adding google verification meta

--- resources/views/partials/metas.blade.php
+++ resources/views/partials/metas.blade.php
@@ -2,4 +2,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="Encuentra lugares para patinar en tu ciudad #skateOrDie. Únete a la comunidad de skate más grande de México.">
 <meta name="keywords" content="Skateparks, Skateparks Guadalajara, Skateparks México, patinar en guadalajara, lugares para patinar, lugares para patinar guadalajara, bowls guadalajara, bowls méxico, bowls, guadalajara, donde patinar en guadalajara, skate spots, skate spots guadalajara, skate spots mexico, skateboarding, skate guadalajara, skateboarding guadalajara, chapultepec, patinar en chapultepec, directorio skateparks, directorio skateparks mexico, skateparks jalisco, urbeskate, lugares para patinar, ollie shit, drop in, instagram, sk8spotsmx">
+<meta name="google-site-verification" content="E04iBql2ry9aAtZXcrBIHUv_v8Y2GxTY8UIgM61fnZ4" />
 <meta name="author" content="Antonio Calderón @FRKodes">
