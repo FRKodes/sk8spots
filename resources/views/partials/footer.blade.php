dae12deefa0417f4467d1a55b2198c7008b47c39
FRKodes <frkalderon@gmail.com>
2018-03-17 21:04:32 -0600

    Ading some credits

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,9 +7,10 @@
 			<li><a href="https://www.facebook.com/sk8SpotsMx/" target="_blank" title="Ver sk8SpotsMx en Facbeook" class="icon-facebook"></a></li>
 		</ul>
 	</div>
-	<div class="col-sm-6 contact-column">
+	<div class="col-sm-6 contact-column text-right">
 		<p>
-			Email de contacto: <a href="mailto:sk8spotsmx@gmail.com" title="Mándamos un correo!">sk8spotsmx@gmail.com</a>
+			Email de contacto: <a href="mailto:sk8spotsmx@gmail.com" title="Mándamos un correo!">sk8spotsmx@gmail.com</a> <br>
+			Desarrollado por: <a href="http://frkodes.ninja" target="_blank">Tony C.J</a>
 		</p>
 	</div>
 </div>
